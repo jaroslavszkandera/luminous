d72b1e95b17c5b686dedeb9d85d0753b31b893b0
Jaroslav Szkandera <jaroslav.szk@gmail.com>
2026-04-09 23:27:15 +0200

    feat: show thread name in log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use color_print::cformat;
+use std::io::Write;
 use std::process;
 
 use luminous::config::Config;
@@ -5,6 +7,32 @@ use luminous::config::Config;
 fn main() {
     let config = Config::load();
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(&config.log))
+        .format(|buf, record| {
+            let level = record.level();
+            let level_char = match level {
+                log::Level::Error => cformat!("<s><red>ERROR</>"),
+                log::Level::Warn => cformat!("<yellow>WARN </>"),
+                log::Level::Info => cformat!("<green>INFO </>"),
+                log::Level::Debug => cformat!("<blue>DEBUG</>"),
+                log::Level::Trace => cformat!("<cyan>TRACE</>"),
+            };
+
+            let curr_thread = std::thread::current();
+            let thread_name = curr_thread.name().unwrap_or("---");
+            let timestamp = chrono::Local::now().format("%T");
+
+            writeln!(
+                buf,
+                "{}{} {} {} {}{} {}",
+                cformat!("<bright-black>[</>"),
+                timestamp,
+                level_char,
+                thread_name,
+                record.module_path().unwrap_or(""),
+                cformat!("<bright-black>]</>"),
+                record.args()
+            )
+        })
         .filter_module("winit", log::LevelFilter::Warn)
         .filter_module("tracing", log::LevelFilter::Warn)
         .filter_module("zbus", log::LevelFilter::Warn)
